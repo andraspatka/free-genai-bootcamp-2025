4fa3df2bfa36df20f98cb2c6e5ed9576a441909b
Patka Zsolt-András <andras.patka.dev@gmail.com>
2025-02-11 19:39:22 +0200

    extract api url to an env var

--- lang-portal/frontend-react/vite.config.ts
+++ lang-portal/frontend-react/vite.config.ts
@@ -2,12 +2,20 @@ import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
+import dotenv from 'dotenv';
+dotenv.config();
+
+const apiUrl = process.env.VITE_API_URL;
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  define: {
+    __API_URL__: JSON.stringify(apiUrl),
   },
   server: {
     proxy: {
