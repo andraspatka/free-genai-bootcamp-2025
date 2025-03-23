b0749247e4f5c0bc1f8f3865a66021b95a8629a6
Zsolt-Andras Patka <andras.patka@gmail.com>
2025-03-23 13:13:16 +0200

    feat: add user data script, fix launch template

--- opea/tf-base-infra/iac/asg.tf
+++ opea/tf-base-infra/iac/asg.tf
@@ -1,16 +1,20 @@
 
 resource "aws_autoscaling_group" "asg" {
-  launch_configuration = aws_launch_configuration.launch_config.id
-  min_size             = 0
-  max_size             = 1
-  desired_capacity     = 0
-  vpc_zone_identifier  = ["subnet-12345678"] # Replace with your subnet ID
+  name = "${local.environment}-${local.project}-asg"
+  launch_template {
+    id      = aws_launch_template.launch_template.id
+    version = "$Latest"
+  }
+  min_size            = 0
+  max_size            = 1
+  desired_capacity    = 0
+  vpc_zone_identifier = [module.vpc.public_subnets[0]]
 
   dynamic "tag" {
     for_each = local.common_tags
     content {
-      key                 = each.key
-      value               = each.value
+      key                 = tag.key
+      value               = tag.value
       propagate_at_launch = true
     }
 
@@ -18,6 +22,22 @@ resource "aws_autoscaling_group" "asg" {
 
   lifecycle {
     ignore_changes = [desired_capacity]
+  }
+}
+
+resource "aws_security_group" "allow_outbound_https" {
+  name        = "${local.environment}-${local.project}-allow-outbound-https"
+  description = "Allow outbound HTTPS connections to AWS service endpoints"
+  vpc_id      = module.vpc.vpc_id
+
+  # Allow outbound connections on port 443
+  egress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0" # Allow outbound to all IPs on port 443
+    ]
   }
 }
 
@@ -44,58 +64,33 @@ resource "aws_iam_role_policy_attachment" "ssm_role_policy" {
 
 # Inspiration for running vllm on EC2
 # https://aws.amazon.com/blogs/machine-learning/serving-llms-using-vllm-and-amazon-ec2-instances-with-aws-ai-chips/
-resource "aws_launch_configuration" "launch_config" {
-  name = "${local.environment}-${local.project}-launch-configuration"
+resource "aws_launch_template" "launch_template" {
+  name = "${local.environment}-${local.project}-launch-template"
   # Deep Learning AMI Neuron (Ubuntu 22.04) 20241221
-  image_id      = "ami-05f04e4216ff500c0"
-  instance_type = "inf2.xlarge"
+  image_id             = "ami-05f04e4216ff500c0"
+  instance_type        = "inf2.xlarge"
+  security_group_names = [aws_security_group.allow_outbound_https.name]
 
-  spot_price = "0.50" # Set your desired max spot price
+  instance_market_options {
+    market_type = "spot"
+    spot_options {
+      max_price = "0.50"
+    }
+  }
 
-  iam_instance_profile = aws_iam_instance_profile.ssm_instance_profile.id
+  iam_instance_profile {
+    name = aws_iam_instance_profile.ssm_instance_profile.name
+  }
 
-  user_data = <<-EOF
-              #!/bin/bash
-              cat > /app/Dockerfile <<'DOCKERFILE_EOF'
-              # default base image
-              ARG BASE_IMAGE="public.ecr.aws/neuron/pytorch-inference-neuronx:2.1.2-neuronx-py310-sdk2.20.0-ubuntu20.04"
-              FROM \$BASE_IMAGE
-              RUN echo "Base image is \$BASE_IMAGE"
-              # Install some basic utilities
-              RUN apt-get update && \\
-                  apt-get install -y \\
-                      git \\
-                      python3 \\
-                      python3-pip \\
-                      ffmpeg libsm6 libxext6 libgl1
-              ### Mount Point ###
-              # When launching the container, mount the code directory to /app
-              ARG APP_MOUNT=/app
-              VOLUME [ \$APP_MOUNT ]
-              WORKDIR \$APP_MOUNT/vllm
-              RUN python3 -m pip install --upgrade pip
-              RUN python3 -m pip install --no-cache-dir fastapi ninja tokenizers pandas
-              RUN python3 -m pip install sentencepiece transformers==4.36.2 -U
-              RUN python3 -m pip install transformers-neuronx --extra-index-url=https://pip.repos.neuron.amazonaws.com -U
-              RUN python3 -m pip install --pre neuronx-cc==2.15.* --extra-index-url=https://pip.repos.neuron.amazonaws.com -U
-              ENV VLLM_TARGET_DEVICE neuron
-              RUN git clone https://github.com/vllm-project/vllm.git && \\
-                  cd vllm && \\
-                  git checkout v0.6.2 && \\
-                  python3 -m pip install -U \\
-                      cmake>=3.26 ninja packaging setuptools-scm>=8 wheel jinja2 \\
-                      -r requirements-neuron.txt && \\
-                  pip install --no-build-isolation -v -e . && \\
-                  pip install --upgrade triton==3.0.0
-              CMD ["/bin/bash"]
-              DOCKERFILE_EOF
-              EOF
+  user_data = filebase64("${path.module}/user_data.sh")
 
-  ebs_block_device {
-    device_name           = "/dev/sdh"
-    volume_size           = 100
-    volume_type           = "gp3"
-    delete_on_termination = true
+  block_device_mappings {
+    device_name = "/dev/sdh"
+    ebs {
+      volume_size           = 100
+      volume_type           = "gp3"
+      delete_on_termination = true
+    }
   }
 
   lifecycle {
@@ -112,6 +107,6 @@ resource "aws_autoscaling_schedule" "scale_down_schedule" {
   autoscaling_group_name = aws_autoscaling_group.asg.name
   scheduled_action_name  = "${local.environment}-${local.project}-scale-down-midnight"
   desired_capacity       = 0
-  start_time             = "2025-03-23T00:00:00Z" # UTC time for midnight
-  recurrence             = "cron(0 0 * * ? *)"    # Every day at midnight UTC
+  start_time             = "2025-03-24T00:00:00Z" # UTC time for midnight
+  recurrence             = "0 0 * * *"            # Every day at midnight UTC
 }
